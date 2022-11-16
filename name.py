b2d982abc25352b290c9a9fec6ced2967dd6e1a8
Misha <mihalelih2017@gmail.com>
2022-11-16 21:46:10 +0200

    datemytime

---
+++ name.py
@@ -0,0 +1,7 @@
+import datetime
+zminna_date_time=datetime.datetime(2022,11,16,hour=21,minute=35,second=34,microsecond=585)
+print(f'object daytine={zminna_date_time}')
+print(f'type-{type(zminna_date_time)}')
+zminna_date_now=datetime.datetime.now()
+print(zminna_date_now)
+print(datetime.datetime.today())
